bd662fe42ae20db6aa1c3e27ece6dc5503f04b4a
Gabriel <gabriel.aguilar@globalcorporation.cc>
2016-04-20 16:56:06 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,54 @@
+var modulo = angular.module("app",["ui.router","ngAnimate"]);
+
+modulo.config(function($stateProvider,$urlRouterProvider){
+	$urlRouterProvider.otherwise("/Inicio")
+
+	$stateProvider
+		.state("Inicio",{
+			url:"/Inicio",
+			templateUrl:"corazon.html"
+		})
+		.state("Mensaje",{
+			url:"/Mensaje",
+			templateUrl:"mensaje.html"
+		})
+		.state("Video",{
+			url:"/Video",
+			templateUrl:"video.html"
+		})
+
+
+})
+
+modulo.controller("AppController",function($scope,$location){
+	console.log("Hola controller",$location.path().slice(1));
+	$scope.TipoVista = $location.path().slice(1)
+	$scope.CambioUrl = function(dato)
+	{
+		$scope.TipoVista = dato;
+	}
+})
+modulo.directive("rotacion",function($timeout){
+	return{
+		link:function(scope,ele,attrs){
+
+			$timeout(function(){
+				ele.find(".cuadrado").toggleClass("rotar");
+			},1000)
+		}
+	}
+})
+modulo.directive("latido",function($timeout){
+	return{
+		link:function(scope,ele,attrs){
+			$timeout(function(){
+				ele.addClass("show");
+			},5000)
+
+			ele.on("click",function(){
+				ele.find("span").toggleClass("icon-play3").toggleClass("icon-stop2").toggleClass("margin-left")
+				$(".Corazon").toggleClass("startLatido")
+			})
+		}
+	}
+})
